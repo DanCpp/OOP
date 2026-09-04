e3d82752817d7bd5929d5b6cb42ff197e570bdf2
DanCpp <d.ushakov3@g.nsu.ru>
2026-09-04 10:54:14 +0700

    made makeHeap function; gonna write some tests

--- Task-1-1-1/src/main/java/org/example/Main.java
+++ Task-1-1-1/src/main/java/org/example/Main.java
@@ -3,15 +3,41 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void repair_from(int[] array, int i) {
+        int size = array.length - i;
+        int it = i;
+        while (true) {
+            int left = 2 * (it - i) + 1;
+            int right = 2 * (it - i) + 2;
+            int highest = it - i;
+
+            if (left < size && array[left + i] > array[highest + i]) {
+                highest = left;
+            }
+            if (right < size && array[right + i] > array[highest + i]) {
+                highest = right;
+            }
+
+            if (highest == (it - i)) {
+                break;
+            }
+
+            int temp = array[it];
+            array[it] = array[highest + i];
+            array[highest + i] = temp;
+
+            it = highest + i;
         }
+    }
+
+    public static void makeHeap(int[] array) {
+        for (int heapTop = array.length - 2; heapTop >= 0; heapTop--) {
+            repair_from(array, heapTop);
+        }
+    }
+
+    public static void heapsort(int[] array) {
+
     }
 }
---
+++ Task-1-1-1/src/test/java/org/example/MainTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void repair_from() {
+    }
+
+    @Test
+    void makeHeap() {
+    }
+
+    @Test
+    void heapsort() {
+    }
+}
