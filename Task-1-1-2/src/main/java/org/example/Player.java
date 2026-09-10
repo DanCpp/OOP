f12c7c4a9196ada453fca6f244e204e0be2e9930
DanCpp <danila.ushakov.08@mail.ru>
2026-09-10 18:22:11 +0700

    Some code for Player

--- Task-1-1-2/src/main/java/org/example/Player.java
+++ Task-1-1-2/src/main/java/org/example/Player.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.Vector;
+
 public class Player {
+    private Vector<Card> hand;
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new Vector<>();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < hand.size(); i++) {
+            stringBuilder.append(hand.get(i).toString());
+            if (i != hand.size() - 1) { stringBuilder.append(", "); }
+        }
+
+        return stringBuilder.toString();
+    }
 }
